ed2ab895e6bbb727a0fcb4c3f86bb21946fb8dc4
agpartha <agpartha@hotmail.com>
2016-02-14 14:54:51 -0800

    Add random data set for each run and sort it so that algorithm can be tested...

--- Sandbox/bsearch.c
+++ Sandbox/bsearch.c
@@ -1,74 +1,96 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int bsearch (int data[], int size, int val)
+
+int comp_int (const void *a, const void *b)
 {
-	static int recurs = 0;
-	int s, m, e;
-
-	s = 0;
-	e = s + size - 1;
-	m = (s + e) / 2 ;
-
-	printf ("recurs(%d): start %d mid %d end %d, val %d\n", recurs, s, m, e, val);
-
-	// check and return if we have the match
-	if (data[m] == val) {
-		recurs--;
-		return m;
-	}
-
-	// if we are the single element, 
-	if (s == e) {
-		recurs--;
+	if (*((int *)a) == *((int *)b))
+		return 0;
+	else if (*((int *)a) < *((int *)b))
 		return -1;
-	}
+	return 1;
+}
 
-	// If data is less than mod, go search from left section
-	// Cannot add if the index was negative still.
-	if (val < data[m]) {
-		int r;
+int b_search (int data[], int size, int val)
+{
+		static int recurs = 0;
+		int s, m, e;
 
-		recurs++;
-		r =  bsearch(&data[s], (m - s), val);
-		if (r >= 0)
-			return s + r;
-	}  
+		s = 0;
+		e = s + size - 1;
+		m = (s + e) / 2 ;
 
-	// data must be greater than mid value and so look in the right side.
-	if (val > data[m]) {
-		int r;
+		printf ("recurs(%d): start %d mid %d end %d, val %d\n", recurs, s, m, e, val);
 
-		recurs++;
-		r =  bsearch(&data[m + 1], (e - m), val);
-		if (r >= 0)
-			return m + 1 + r;
-	}  
+		// check and return if we have the match
+		if (data[m] == val) {
+				recurs--;
+				return m;
+		}
 
-	return -1;
+		// if we are the single element, 
+		if (s == e) {
+				recurs--;
+				return -1;
+		}
+
+		// If data is less than mod, go search from left section
+		// Cannot add if the index was negative still.
+		if (val < data[m]) {
+				int r;
+
+				recurs++;
+				r =  b_search(&data[s], (m - s), val);
+				if (r >= 0)
+						return s + r;
+		}  
+
+		// data must be greater than mid value and so look in the right side.
+		if (val > data[m]) {
+				int r;
+
+				recurs++;
+				r =  b_search(&data[m + 1], (e - m), val);
+				if (r >= 0)
+						return m + 1 + r;
+		}  
+
+		return -1;
 }
 
 
 void main (void)
 {
-	int data[] = { 10, 20, 30, 400, 533, 789, 1000, 2000, 2011};
-	int i, val, size = sizeof(data)/sizeof(data[0]);
-	
-	i = 0;
-	while (i < size) {
-		printf("Data[%2d] : %3d\n", i, data[i]);
-		i++;
-	}
+		int data[] = { 10, 20, 30, 400, 533, 789, 1000, 2000, 2011};
+		int i, val, size = sizeof(data)/sizeof(data[0]);
 
-	printf("\n\nPlease Enter the number to search in the above data ? \n");
-	scanf("%d", &val);
+		while (1) {
 
-	printf("\n\nSearching %d ....\n\n", val);
+				i = 0;
+				while (i < size) {
+						data[i] = data[i] + (rand() % 2321);
+						i++;
+				}
+			
+				qsort(data, sizeof(data)/sizeof(data[0]), sizeof(data[0]), comp_int);
 
-	i = bsearch(data, size, val);
+				i = 0;
+				while (i < size) {
+						printf("Data[%2d] : %3d\n", i, data[i]);
+						i++;
+				}
 
-	if (i < 0)
-		printf("\n\nCould not find %d ....\n\n", val);
-	else 
-		printf("\n\nFound %d at position %d\n\n", val, i);
+				printf("\n\nPlease Enter the number to search in the above data ? \n");
+				scanf("%d", &val);
+
+				printf("\n\nSearching %d ....\n\n", val);
+
+				i = b_search(data, size, val);
+
+				if (i < 0)
+						printf("\n\nCould not find %d ....\n\n", val);
+				else 
+						printf("\n\nFound %d at position %d\n\n", val, i);
+		}
 }
